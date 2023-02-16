5bce0327be3d54be12b39e10c52e971c4f0a8a73
Darkmatter85 <103216160+Darkmatter85@users.noreply.github.com>
2023-02-16 23:36:38 +0530

    Update setup.sh

--- scripts/setup.sh
+++ scripts/setup.sh
@@ -2,11 +2,8 @@
 
 set -e
 
-until /root/.dotnet/tools/dotnet-ef database update --no-build; do
->&2 echo "SQL Server is starting up"
+until dotnet ef database update --no-build; do >&2 echo "SQL Server is starting up"
 sleep 1
 done
-
 >&2 echo "SQL Server is up - executing command"
-
-/root/.dotnet/tools/dotnet-ef database update
+dotnet ef database update
